f0d9b43bdab0aa8597a3dc7b66ae43fc01798d03
pyvyx <pyvyx@web.de>
2023-11-08 14:04:21 +0100

    Changed layout from vertical to horizontal

--- QR-Generator/src/main.cpp
+++ QR-Generator/src/main.cpp
@@ -20,11 +20,10 @@ int main()
         ImVec2 newPos = window.Pos();
         const float windowWidth = window.Size().x;
         const float windowHeight = window.Size().y;
-        float previousHeight = 0;
 
         {
             ImGui::SetNextWindowViewport(ImGui::GetMainViewport()->ID);
-            ImGui::SetNextWindowSize({ windowWidth, -1 });
+            ImGui::SetNextWindowSize({ windowWidth/2, windowHeight });
             ImGui::SetNextWindowPos(newPos);
 
             ImGui::Begin("InputWindow", nullptr, IMGUI_WINDOW_FLAGS);
@@ -33,7 +32,7 @@ int main()
             {
                 int border = 2;
                 qrcodegen::QrCode qr = qrcodegen::QrCode::encodeText(s.c_str(), qrcodegen::QrCode::Ecc::LOW);
-                img.Assign(qr, border, (size_t)(windowHeight - ImGui::GetWindowHeight()) / (qr.getSize() + border*2));
+                img.Assign(qr, border, (size_t)(windowWidth - ImGui::GetWindowWidth()) / (qr.getSize() + border*2));
             }
 
             static int eccLevel = 0;
@@ -52,21 +51,21 @@ int main()
             ImGui::SetNextItemWidth(100.f);
             ImGui::InputInt("Skalierung", &scale, 1, 10, ImGuiInputTextFlags_CharsDecimal);
 
-            newPos.y += ImGui::GetWindowHeight();
-            previousHeight = ImGui::GetWindowHeight();
+            newPos.x += ImGui::GetWindowWidth();
             ImGui::End();
         }
 
         {
             ImGui::SetNextWindowViewport(ImGui::GetMainViewport()->ID);
-            ImGui::SetNextWindowSize({ windowWidth, windowHeight - previousHeight });
+            ImGui::SetNextWindowSize({ windowWidth / 2, windowHeight });
             ImGui::SetNextWindowPos(newPos);
             ImGui::Begin("QRCodeImage", nullptr, IMGUI_WINDOW_FLAGS);
-
-            const float imgSize = std::min(ImGui::GetWindowHeight(), ImGui::GetWindowWidth()) - 2*ImGui::GetStyle().WindowPadding.y;
-            const float xPos = (windowWidth - imgSize) / 2.f;
+        
+            const float imgSize = std::min(ImGui::GetWindowHeight(), ImGui::GetWindowWidth()) - 2*std::max(ImGui::GetStyle().WindowPadding.x, ImGui::GetStyle().WindowPadding.y);
+            const float xPos = (ImGui::GetWindowWidth() - imgSize) / 2.f;
+            const float yPos = (ImGui::GetWindowHeight() - imgSize) / 2.f;
             const ImVec2 p = ImGui::GetCursorScreenPos();
-            ImGui::SetCursorScreenPos(ImVec2(p.x + xPos, p.y));
+            ImGui::SetCursorScreenPos(ImVec2(p.x + xPos, p.y + yPos));
             ImGui::Image((void*)(intptr_t)img.GetGpuImage(), { imgSize, imgSize });
             ImGui::End();
         }
